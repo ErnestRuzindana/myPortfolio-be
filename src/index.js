3112649cb75fd8c172c886849597a667bb5ec466
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-01-11 19:01:57 -0800

    some improvements

--- src/index.js
+++ src/index.js
@@ -35,7 +35,10 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: "10mb", extended: true}))
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
+
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser('random'));
 
